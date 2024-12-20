18d73ad4ffee5ff692e9395353c1d0a11e6fb9b6
engr-mahmudul <dev.mahmudulhasan@gmail.com>
2024-12-20 23:11:00 +0200

    variables, array and basic object

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,16 @@
 "use strict";
 let a = 5;
-let b = 6;
-console.log(a / b);
+let b = "Test";
+let loading;
+loading = true;
+let age; //union type
+age = 18;
+age = "18";
+let array_1 = ["1", "2", "3"];
+let array_2 = [1, "2", "3"];
+let array_3 = [true, 0];
+let obj_1 = {
+    a: 5,
+    b: 6,
+    c: '7'
+};
